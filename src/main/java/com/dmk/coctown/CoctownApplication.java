00c9f0d39aa58d5f530465c269e1121671db87bc
Dongmin Kim <ddmkim94@gmail.com>
2024-05-22 16:37:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/dmk/coctown/CoctownApplication.java
@@ -0,0 +1,13 @@
+package com.dmk.coctown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmk/coctown/CoctownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmk.coctown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
