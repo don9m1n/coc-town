46b1c6f4e8bae3d0bc8b81eda973e5f7d6a889e8
Dongmin Kim <ddmkim94@gmail.com>
2024-05-30 19:30:19 +0900

    Feat: 회원 도메인 설계

---
+++ src/main/java/com/dmk/coctown/domain/member/model/Member.java
@@ -0,0 +1,51 @@
+package com.dmk.coctown.domain.member.model;
+
+import com.dmk.coctown.common.model.BaseEntity;
+import com.dmk.coctown.domain.member.dto.MemberJoinRequest;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private String profileImg;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
+
+    @Builder
+    private Member(String email, String password, String nickname, String profileImg, MemberRole memberRole) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImg = profileImg;
+        this.memberRole = memberRole;
+    }
+
+    public static Member of(MemberJoinRequest request, String encodedPassword) {
+        return Member.builder()
+                .email(request.getEmail())
+                .password(encodedPassword)
+                .nickname(request.getNickname())
+                .profileImg(null) // TODO: 프로필 이미지 업로드 구현시 수정 필요.
+                .build();
+    }
+
+}
---
+++ src/main/java/com/dmk/coctown/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.dmk.coctown.domain.member.repository;
+
+import com.dmk.coctown.domain.member.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
