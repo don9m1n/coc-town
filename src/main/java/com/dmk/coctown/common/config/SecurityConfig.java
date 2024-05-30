9f790eadcb7ff2a311eed9c439d7abfcebc00586
Dongmin Kim <ddmkim94@gmail.com>
2024-05-30 19:30:02 +0900

    Feat: 시큐리티 설정 작성
    - 개발 편의를 위해 모든 요청에 대한 접근 허용
    - PasswordEncoder 빈 등록

---
+++ src/main/java/com/dmk/coctown/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.dmk.coctown.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
